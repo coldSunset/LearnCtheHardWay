e8ea043bd590344f19dbad32fa18401c16569b98
Ravindu Sirimanna <ravsiriman@gmail.com>
2020-09-02 14:55:37 +1000

    ex24-Input Output Files

--- ex24/ex24.c
+++ ex24/ex24.c
@@ -26,11 +26,13 @@ int main(int argc, char* argv[])
 	char* in = NULL; 
 
 	printf("What's your First Name? "); 
-	in = fgets(you.first_name, MAX_DATA -1, stdin); 
+	//in = fgets(you.first_name, MAX_DATA -1, stdin); 
+	in = fscanf(stdin, "%50s", you.first_name);
 	check(in != NULL, "Failed to read first name."); 
 
 	printf("What's your Last Name? "); 
-	in = fgets(you.last_name, MAX_DATA - 1, stdin); 
+	//in = fgets(you.last_name, MAX_DATA - 1, stdin);
+	in = fscanf(stdin, "%50s", you.last_name); 
 	check( in != NULL, "Failed to read last name."); 
 
 	printf("How old are you?");
@@ -51,7 +53,17 @@ int main(int argc, char* argv[])
 	you.eyes = eyes -1; 
 	check(you.eyes >= OTHER_EYES && you.eyes >= 0, "Do it right, that's not an option. "); 
 
-	
+	printf("How much do you make an hour? "); 
+	rc = fscanf(stdin, "%f", &you.income); 
+	check(rc >0, "enter a floating point number"); 
+
+	printf("---- RESULTS -----\n"); 
+
+	printf("First Name: %s", you.first_name); 
+	printf("Last Name: %s", you.last_name); 
+	printf("Age: %d\n", you.age); 
+	printf("Eyes: %s\n", EYE_COLOUR_NAMES[you.eyes]); 
+	printf("Income: %f\n", you.income); 
 
 	return 0; 
 
