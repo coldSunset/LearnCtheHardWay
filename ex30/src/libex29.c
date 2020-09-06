b6ff86b101ce3c301a58e4b45749c5c160c429ea
coldSunset <ravindusiri@gmail.com>
2020-09-06 16:47:35 +1000

    ex30

---
+++ ex30/src/libex29.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "dbg.h"
+#include <string.h> 
+
+int print_a_message(const char* msg)
+{
+	printf("A STRING: %s\n", msg); 
+
+	return 0; 
+}
+
+int uppercase(const char* msg)
+{
+	int i =0 ; 
+	int msg_len = strlen(msg); 
+	//BUG: \0 termination problems 
+	for(i=0; i<msg_len; i++)
+	{
+		printf("%c", toupper(msg[i])); 
+	}
+
+	printf("\n"); 
+ 
+	return 0; 
+}
+
+int lowercase(const char* msg)
+{
+	int i =0; 
+	int msg_len = strlen(msg); 
+	//BUG: \0 termination problems 
+	for(i=0; i < msg_len; i++)
+	{
+		printf("%c", tolower(msg[i])); 
+	}
+
+	printf("\n"); 
+
+	return 0; 
+}
+
+int fail_on_purpose(const char* msg)
+{
+	return 1; 
+}
